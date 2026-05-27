1bfa356860fd8e747b5f499b05ed2d851e257af9
pradeepkambalapally <pradeepkambalapally2004@gmail.com>
2026-05-28 00:33:40 +0530

    Configure cloud MySQL database integration

--- config/db.js
+++ config/db.js
@@ -4,16 +4,23 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
+
     if (err) {
+
         console.log("Database connection failed");
         console.log(err);
+
     } else {
+
         console.log("MySQL Connected");
+
     }
+
 });
 
 module.exports = connection;
